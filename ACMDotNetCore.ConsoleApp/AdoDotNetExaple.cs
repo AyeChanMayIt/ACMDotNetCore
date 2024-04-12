09880deffe66c6ac3dcab1eacb47f14d15d838b1
Aye Chan May IT <ayechanmayit@ayabank.com>
2024-04-12 15:11:48 +0630

    - ADO.Net Create, Update[Done]

---
+++ ACMDotNetCore.ConsoleApp/AdoDotNetExaple.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExaple
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder=new SqlConnectionStringBuilder()
+        {
+            DataSource = ".", //server name
+            InitialCatalog = "ACMDotNetDB",
+            UserID = "sa",
+            Password = "aya123"      
+        };
+        public void Read()
+        {
+            /*SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder.DataSource = "RV-IT-LP-130"; //server name
+            stringBuilder.InitialCatalog = "ACMDotNetDB";
+            stringBuilder.UserID = "sa";
+            stringBuilder.Password = "aya123";*/
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            Console.WriteLine("Connection is open");
+
+            string query = "select * From Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);  // for running query 
+            DataTable dt = new DataTable(); //create result table
+            sqlDataAdapter.Fill(dt); //accept result table
+
+            connection.Close();
+            Console.WriteLine("Connection is close");
+
+            //dataset > datatable > data row > data col:
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog ID =>" + dr["BlogId"]);
+                Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+                Console.WriteLine("----------------------------");
+            }
+
+        }
+        public void Create(string title,string author,string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                           ([BlogTitle]
+                           ,[BlogAuthor]
+                           ,[BlogContent])
+                     VALUES
+                           (@BlogTitle, 
+                           @BlogAuthor,
+                           @BlogContent)";
+            SqlCommand cmd = new SqlCommand(query,connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            string message = result > 0 ? "Saving Sucessful" : "Saving Error";
+            Console.WriteLine(message);     
+            connection.Close();
+        }
+        public void Update(int id,string title,string author,string content) 
+        {
+            SqlConnection connection=new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+                               SET [BlogTitle] = @BlogTitle,
+	                               [BlogAuthor] =@BlogAuthor,
+	                               [BlogContent] =@BlogContent
+                             WHERE BlogId=@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            string message = result > 0 ? "Update Sucessful" : "Update Error";
+            Console.WriteLine(message);
+            connection.Close();
+        }
+    }
+}
--- ACMDotNetCore.ConsoleApp/Program.cs
+++ ACMDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
 ﻿
+using ACMDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+//Ctrl + . >> Suggestion
+//F9 >> Break Point
+
+// => C# <=> DB
+
+AdoDotNetExaple adoDotNetExaple = new AdoDotNetExaple();
+//adoDotNetExaple.Read();
+//adoDotNetExaple.Create("Funny", "Mr.Bean", "Merry Cherist");
+adoDotNetExaple.Update(1102, "Thingyan", "Test Auhtour", "Thingyan Festival");
