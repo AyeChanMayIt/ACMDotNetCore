f020a559c9ed4cb91888376638acf110199608c0
ayechanmayit <ayechanmayit2929@gmail.com>
2024-06-22 17:19:53 +0630

    ACMDotNetCore.MinimalAPI [Complete]

---
+++ ACMDotNetCore.MinimalAPI/Db/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ACMDotNetCore.ConsoleApp.Services;
+using ACMDotNetCore.MinimalAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMDotNetCore.MinimalAPI.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+      
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ ACMDotNetCore.MinimalAPI/Program.cs
@@ -0,0 +1,34 @@
+using ACMDotNetCore.MinimalAPI.Db;
+using ACMDotNetCore.MinimalAPI.Feacture.Blog;
+using ACMDotNetCore.MinimalAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Formats.Asn1;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(otp =>
+{
+    otp.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+  ServiceLifetime.Transient,
+  ServiceLifetime.Transient);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapGet("/",() => "Hello World");
+//BlogService.MapBlgs(app);
+//reverse use this keyword in IEndpoint...//
+app.MapBlgs(); // this are call extension method
+app.Run();
+
